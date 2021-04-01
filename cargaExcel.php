afa3f2627b1965eaf811388cbe817f420593bbc2
Fed Lle <fedelleos@gmail.com>
2021-04-01 13:15:22 +0100

    commit

--- cargaExcel.php
+++ cargaExcel.php
@@ -8,17 +8,17 @@ $message='';
 
 try {
     if ($_FILES["import_excel"]["name"] != '') {
+        $file = $_FILES["import_excel"]["name"];
         $allowed_extension = array('xls', 'csv', 'xlsx');
-        $file_array = explode(".", $_FILES["import_excel"]["name"]);
+        $file_array = explode(".", $file);       
         $file_extension = end($file_array);
-
-        if (in_array($file_extension, $allowed_extension)) {
-            //$file_name = time() . '.' . $file_extension;
-            $file_name = 'uploads/' . $_FILES["import_excel"]["name"];
-            move_uploaded_file($_FILES['import_excel']['tmp_name'], $file_name);
+        $file_precuel = prev($file_array);
+        if (in_array($file_extension, $allowed_extension)) {            
+            $file_name = 'uploads/' . $file;          
+            move_uploaded_file($_FILES['import_excel']['tmp_name'], $file_name);         
             $file_type = \PhpOffice\PhpSpreadsheet\IOFactory::identify($file_name);
             $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($file_type);
-            $spreadsheet = $reader->load($file_name);
+            $spreadsheet = $reader->load($file_name);                      
             if ($spreadsheet != null) {
                 //Hola excel 0
                 $hojaActual = $spreadsheet->getSheet(0);
--- import.php
+++ import.php
@@ -6,50 +6,71 @@ include 'cargaExcel.php';
 
 $b = $_GET['b'];
 
+$entidad = $campos = $bvalues = $i = $j = $k = $n = $o = null;
+$camposr = $bvaluesr = $values = [];
+
 switch ($b) {
-    case 'bbva':        bbva();         break;
-    case 'caixa':       caixa();        break;
-    case 'santander':   santander();    break;
+    case 'bbva':  
+        $entidad = array('Fecha Contable','Fecha valor','Código','Concepto','Observaciones','Importe','Saldo','Divisa','Oficina','Remesa');
+        $campos = array('fecha_contable', 'fecha_valor', 'codigo', 'concepto', 'observaciones', 'importe', 'saldo', 'divisa', 'oficina', 'remesa');
+        $bvalues = array(':fecha_contable', ':fecha_valor', ':codigo', ':concepto', ':observaciones', ':importe', ':saldo', ':divisa', ':oficina', ':remesa');
+        $l=17; $m=2;    //fila y columna donde empiezan los datos en la hoja
+        $o = 2;         //restarle a $j para fecha_contable en $campos
+        $k = 2;         //columna dondo fecha_contable
+        importa();         
+        break;
+    case 'caixa':       
+        $entidad = array('Movimiento','Fecha','Fecha valor', 'Más datos','Importe','Saldo');
+        $campos = array('concepto', 'fecha_contable', 'fecha_valor', 'observaciones', 'importe', 'saldo');
+        $bvalues = array(':concepto', ':fecha_contable', ':fecha_valor', ':observaciones', ':importe', ':saldo');     
+        $l=4; $m=1;    //fila y columna donde empiezan los datos en la hoja
+        $o = 1;         //dondo fecha_contable en $campos
+        $k = 2;        //columna dondo fecha_contable
+        importa();         
+        break;
+    case 'santander':   
+        $entidad = array('FECHA OPERACIÓN','FECHA VALOR','CONCEPTO','IMPORTE EUR','SALDO');
+        $campos = array('fecha_contable', 'fecha_valor', 'concepto', 'importe', 'saldo');
+        $bvalues = array( ':fecha_contable', ':fecha_valor', ':concepto', ':importe', ':saldo');      
+        $l=9; $m=1;    //fila y columna donde empiezan los datos en la hoja
+        $o = 1;         //dondo fecha_contable en $campos
+        $k = 1;        //columna dondo fecha_contable
+        importa();         
+        break;
 }
 
-function santander()
-{
-    $entidad = array('FECHA OPERACIÓN','FECHA VALOR','CONCEPTO','IMPORTE EUR','SALDO');
-    $campos = array('fecha_contable', 'fecha_valor', 'concepto', 'importe', 'saldo');
-    $bvalues = array( ':fecha_contable', ':fecha_valor', ':concepto', ':importe', ':saldo');
-    global $csize,$rsize,$hojaActual,$message;
-    //Recuperamos campos de excel similares a la tabla mysql e insertamos en tabla
-    $camposr = [];//Campos de la fila no vacios (reales)
-    $bvaluesr = [];//Campos de bindeo no vacios (reales)
-    $values = []; //Array de inserción
-    $celda = null;
-    $valorform = null; //Valor de la celda
-    $m = 0; $i=9; $j=1;
-    if (verificaArchivo($i-1,$j,$csize,$entidad,$hojaActual)) {   //Comprobamos si el fichero corresponde con el banco
-        for ($i=9; $i <= $rsize; $i++) {
-            for ($j=1; $j <= $csize; $j++) {          
+function importa() {   
+    global $entidad,$campos,$bvalues,$l,$m,$k,$n,$o,$camposr,$bvaluesr,$values,$b,$csize,$rsize,$hojaActual,$message;
+    //Recuperamos campos de excel similares a la tabla mysql e insertamos en tabla   
+    $archivo_correcto =  verificaArchivo($l-1,$m,$csize,$entidad,$hojaActual);//Comprobamos si el fichero corresponde con el banco
+    if ($archivo_correcto) {   
+        for ($i=$l; $i <= $rsize; $i++) {
+            for ($j=$m; $j <= $csize; $j++) {          
                 //Valor de la celda
                 $celda = $hojaActual->getCellByColumnAndRow($j, $i);
                 $valorform = $celda->getValue();
                 //A partir de esta fila están los valores a insertar, actualizamos los campos necesarios también.
-                if ($valorform != '') {
-                
+                if ($valorform != '') {             
                     //Recuperamos valores de los movimientos de excel, los añadimos a $ivalues [clave][valor]
-                    if ($j == 1) {
-                        $valorform = $celda->getFormattedValue();
-                        $values[':fecha_contable'] = date("Y-m-d", strtotime($valorform));
-                        $camposr[$m] = $campos[$j-1];
-                    } elseif ($j == 2) {
+                    if ($j == $k) {
+                        if($b != 'bbva'){
+                            $valorform = $celda->getFormattedValue();
+                            $values[':fecha_contable'] = date("Y-m-d", strtotime($valorform));
+                        } else {
+                            $values[':fecha_contable'] = date("Y-m-d", strtotime(str_replace('/', '-', $valorform)));
+                        }
+                        $camposr[$n] = $campos[$j-$o];
+                    } elseif ($j == ($k+1)) {
                         $valorform = $celda->getFormattedValue();
                         $values[':fecha_valor'] = date("Y-m-d", strtotime($valorform));
-                        $camposr[$m] = $campos[$j-1];
+                        $camposr[$n] = $campos[$j-$o];
                     } else {
-                        $values[$bvalues[$j-1]] = $valorform;
-                        $camposr[$m] = $campos[$j-1];
+                        $values[$bvalues[$j-$o]] = $valorform;
+                        $camposr[$n] = $campos[$j-$o];
                     }
-                    $bvaluesr[$m] = $bvalues[$j-1];
+                    $bvaluesr[$n] = $bvalues[$j-$o];
                 }          
-                $m++;
+                $n++;
                 if ($j == $csize) {                    // Insertamos en tabla
                 $camposs = implode(", ", $camposr);    //Nos da un string
                 $bvaluess = implode(", ", $bvaluesr);
@@ -57,7 +78,7 @@ function santander()
                         //Campo único
                         $camposs = $camposs.", referencia";
                         $bvaluess = $bvaluess.", :referencia";
-                        $values[':referencia'] = 'santander';
+                        $values[':referencia'] = $b;
                         foreach ($values as $val) {
                             $values[':referencia'] = $values[':referencia'].$val;
                         }
@@ -68,144 +89,7 @@ function santander()
                             $i = $rsize;
                         }
                     }
-                    $m = 0;
-                    $camposr = [];
-                    $bvaluesr = [];
-                    $values = [];
-                }
-            }
-        }
-    } else {
-        $message = '<div class="alert alert-danger">El archivo no se corresponde con la entidad bancaria</div>';
-    }
-}
-
-
-function caixa()
-{
-    $entidad = array('Movimiento','Fecha','Fecha valor', 'Más datos','Importe','Saldo');
-    $campos = array('concepto', 'fecha_contable', 'fecha_valor', 'observaciones', 'importe', 'saldo');
-    $bvalues = array(':concepto', ':fecha_contable', ':fecha_valor', ':observaciones', ':importe', ':saldo');
-    global $csize,$rsize,$hojaActual,$message;
-    //Recuperamos campos de excel similares a la tabla mysql e insertamos en tabla
-    $camposr = [];//Campos de la fila no vacios (reales)
-    $bvaluesr = [];//Campos de bindeo no vacios (reales)
-    $values = []; //Array de inserción
-    $celda = null;
-    $valorform = null; //Valor de la celda
-    $m = 0; $i = 4; $j = 1;
-    if (verificaArchivo($i-1,$j,$csize,$entidad,$hojaActual)) {   //Comprobamos si el fichero corresponde con el banco
-        for ($i=4; $i <= $rsize; $i++) {
-            for ($j=1; $j <= $csize; $j++) {
-                //Valor de la celda
-                $celda = $hojaActual->getCellByColumnAndRow($j, $i);
-                $valorform = $celda->getValue();
-                //A partir de esta fila están los valores a insertar, actualizamos los campos necesarios también.
-                if ($valorform != '') {
-                    //Recuperamos valores de los movimientos de excel, los añadimos a $ivalues [clave][valor]
-                    if ($j == 2) {
-                        $valorform = $celda->getFormattedValue();
-                        $values[':fecha_contable'] = date("Y-m-d", strtotime(str_replace('/', '-', $valorform)));
-                        $camposr[$m] = $campos[$j-1];
-                    } elseif ($j == 3) {
-                        $valorform = $celda->getFormattedValue();
-                        $values[':fecha_valor'] = date("Y-m-d", strtotime(str_replace('/', '-', $valorform)));
-                        $camposr[$m] = $campos[$j-1];
-                    } else {
-                        $values[$bvalues[$j-1]] = $valorform;
-                        $camposr[$m] = $campos[$j-1];
-                    }
-                    $bvaluesr[$m] = $bvalues[$j-1];
-                }
-                $m++;
-                if ($j == $csize) {
-                    // Insertamos en tabla
-                $camposs = implode(", ", $camposr);    //Nos da un string
-                $bvaluess = implode(", ", $bvaluesr);
-                    if ($values != null) {
-                        //Campo único
-                        $camposs = $camposs.", referencia";
-                        $bvaluess = $bvaluess.", :referencia";
-                        $values[':referencia'] = 'lacaixa';
-                        foreach ($values as $val) {
-                            $values[':referencia'] = $values[':referencia'].$val;
-                        }
-                        //Insertamos
-                        $res = insert($camposs, $bvaluess, $values);
-                        if (!$res) {
-                            $message = '<div class="alert alert-danger">El archivo ya existe</div>';
-                            $i = $rsize;
-                        }
-                    }
-                    $m = 0;
-                    $camposr = [];
-                    $bvaluesr = [];
-                    $values = [];
-                } 
-            }
-        }
-    } else {
-        $message = '<div class="alert alert-danger">El archivo no se corresponde con la entidad bancaria</div>';
-    }
-}
-           
-
-function bbva()
-{
-    $entidad = array('Fecha Contable','Fecha valor','Código','Concepto','Observaciones','Importe','Saldo','Divisa','Oficina','Remesa');
-    $campos = array('fecha_contable', 'fecha_valor', 'codigo', 'concepto', 'observaciones', 'importe', 'saldo', 'divisa', 'oficina', 'remesa');
-    $bvalues = array(':fecha_contable', ':fecha_valor', ':codigo', ':concepto', ':observaciones', ':importe', ':saldo', ':divisa', ':oficina', ':remesa');
-    global $csize,$rsize,$hojaActual,$message;
-    //Recuperamos campos de excel similares a la tabla mysql e insertamos en tabla
-    $camposr = [];//Campos de la fila no vacios (reales)
-    $bvaluesr = [];//Campos de bindeo no vacios (reales)
-    $values = []; //Array de inserción
-    $celda = null;
-    $valorform = null; //Valor de la celda
-    $m = 0; $i = 17;   $j = 2;
-    if (verificaArchivo($i-1,$j,$csize,$entidad,$hojaActual)) {   //Comprobamos si el fichero corresponde con el banco
-        for ($i=17; $i <= $rsize; $i++) {
-            for ($j=2; $j <= $csize; $j++) {
-                //Valor de la celda
-                $celda = $hojaActual->getCellByColumnAndRow($j, $i);
-                $valorform = $celda->getValue();
-                //A partir de esta fila están los valores a insertar, actualizamos los campos necesarios también.
-                if ($valorform != '') {
-                    //Recuperamos valores de los movimientos de excel, los añadimos a $ivalues [clave][valor]
-                    if ($j == 2) {
-                        $values[':fecha_contable'] = date("Y-m-d", strtotime(str_replace('/', '-', $valorform)));
-                        $camposr[$m] = $campos[$j-2];
-                    } elseif ($j == 3) {
-                        $values[':fecha_valor'] = date("Y-m-d", strtotime(str_replace('/', '-', $valorform)));
-                        $camposr[$m] = $campos[$j-2];
-                    } else {
-                        $values[$bvalues[$j-2]] = $valorform;
-                        $camposr[$m] = $campos[$j-2];
-                    }
-                    $bvaluesr[$m] = $bvalues[$j-2];
-                }
-                $m++;
-                if ($j == $csize) {
-                    // Insertamos en tabla
-                        $camposs = implode(", ", $camposr);    //Nos da un string
-                        $bvaluess = implode(", ", $bvaluesr);
-                    if ($values != null) {
-                        //Campo único
-                        $camposs = $camposs.", referencia";
-                        $bvaluess = $bvaluess.", :referencia";
-                        $values[':referencia'] = 'bbva';
-                        foreach ($values as $val) {
-                            $values[':referencia'] = $values[':referencia'].$val;
-                        }
-                        //Insertamos
-                        $res = insert($camposs, $bvaluess, $values);
-                        if (!$res) {
-                            $message = '<div class="alert alert-danger">El archivo ya existe</div>';
-                            $i = $rsize;
-                        }
-                    }
-                    $l = 0;
-                    $m = 0;
+                    $n = 0;
                     $camposr = [];
                     $bvaluesr = [];
                     $values = [];
@@ -229,17 +113,6 @@ function verificaArchivo($i,$j,$csize,$caixa,$hojaActual) {
     }  
     return $ver;
 }  
-
-
-function exceptions_error_handler($severity, $message, $filename, $lineno)
-{
-    if (error_reporting() == 0) {
-        return;
-    }
-    if (error_reporting() & $severity) {
-        throw new ErrorException($message, 0, $severity, $filename, $lineno);
-    }
-}
 
 echo $message;
   
