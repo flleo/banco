f1a272065da7e2da357742d749c425c7fc03cd78
root <admin@fedelleos.es>
2023-09-16 17:22:45 +0000

    updaf

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 
 function connect() 
 {
-    $connect = new PDO("mysql:host=localhost;dbname=timado1p_banco", "root", "");
+    $connect = new PDO("mysql:host=localhost;dbname=timado1p_banco", "timado1", "timado1p");
     //$connect = new PDO("mysql:host=localhost;dbname=timado1p_banco", "timado1p_usuario", "Practicas2021");
     //$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
